126fd91f33aa82550ed036cd816d053129dfef2b
Karl Sigel <heykarl@gmail.com>
2021-06-03 14:25:47 -0700

    failing test for add function

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // import functions
 
 // reference needed DOM elements
+const addxInput = document.getElementById('add-x-input');
+const addyInput = document.getElementById('add-y-input');
+const addButton = document.getElementById('add-btn');
+const addResult = document.getElementById('add-result');
 
 
 // set event listeners 
+addButton.addEventListener('click', () => {
+  console.log('add button clicked');
+});
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
